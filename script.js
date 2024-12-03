f01edbf6051d6c8b2a08012fae0ecc08af8d7d1f
dxshx <dishahegde78@gmail.com>
2024-12-03 11:38:27 +0530

    commit 5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000';
+const API_URL = 'http://192.168.25.237:3000';
 let token = localStorage.getItem('token');
 let currentUser = localStorage.getItem('currentUser');
 
